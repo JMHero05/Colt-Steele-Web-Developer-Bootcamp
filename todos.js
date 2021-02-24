e51d1d4f370dbfe44a0c5cb68c2e52ef801153cb
Jake Mills <jake.mills.05@gmail.com>
2021-02-24 18:30:34 -0500

    Created List all todos function

--- todos.js
+++ todos.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let response, newTodo;
   let list = [];
   const initQuestion = 'What would you like to do?';
+  const pageBreak = '********************************';
 
   const todoList = () => {
     response = prompt(initQuestion).toLowerCase();
@@ -9,6 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log('Top of while loop');
       if (response === 'new') {
         addATodo();
+      } else if (response === 'list') {
+        listTodos();
       }
     }
   };
@@ -17,6 +20,15 @@ document.addEventListener('DOMContentLoaded', () => {
     newTodo = prompt('Enter new todo');
     list.push(newTodo);
     console.log(`${newTodo} added to list`);
+    response = prompt(initQuestion).toLowerCase();
+  };
+
+  const listTodos = () => {
+    console.log(pageBreak);
+    for (let [i, todo] of list.entries()) {
+      console.log(`%d: %s`, i, todo);
+    }
+    console.log(pageBreak);
     response = prompt(initQuestion).toLowerCase();
   };
 
