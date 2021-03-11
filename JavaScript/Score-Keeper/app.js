4647341d54fab3c81ad4077310fa6ba6e48c5998
Jake Mills <jake.mills.05@gmail.com>
2021-03-10 21:20:20 -0500

    Incrementing count on button push for both players

--- JavaScript/Score-Keeper/app.js
+++ JavaScript/Score-Keeper/app.js
@@ -1,10 +1,15 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 
+let p1Score = 0;
+let p2Score = 0;
+
 p1Button.addEventListener('click', () => {
-  console.log('click P1');
+  p1Score++;
+  document.querySelector('#p1Score').innerText = p1Score;
 });
 
 p2Button.addEventListener('click', () => {
-  console.log('click P2');
+  p2Score++;
+  document.querySelector('#p2Score').innerText = p2Score;
 });
