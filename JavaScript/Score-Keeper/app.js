45755a1400f454955e118b2e22ee0321c0f52c04
Jake Mills <jake.mills.05@gmail.com>
2021-03-10 21:25:26 -0500

    Added final score logic for both buttons

--- JavaScript/Score-Keeper/app.js
+++ JavaScript/Score-Keeper/app.js
@@ -1,15 +1,30 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 
+let p1Display = document.querySelector('#p1Score');
+let p2Display = document.querySelector('#p2Score');
+
 let p1Score = 0;
 let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click', () => {
-  p1Score++;
-  document.querySelector('#p1Score').innerText = p1Score;
+  if (!isGameOver) {
+    p1Score++;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+    }
+    p1Display.textContent = p1Score;
+  }
 });
 
 p2Button.addEventListener('click', () => {
-  p2Score++;
-  document.querySelector('#p2Score').innerText = p2Score;
+  if (!isGameOver) {
+    p2Score++;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+    }
+    p2Display.textContent = p2Score;
+  }
 });
