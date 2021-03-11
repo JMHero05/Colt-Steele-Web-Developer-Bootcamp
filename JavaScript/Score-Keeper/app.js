b701e3e2dbf71a3090129f4dea91fc35729176e2
Jake Mills <jake.mills.05@gmail.com>
2021-03-10 21:12:27 -0500

    Selected p1 p2 buttons and added event listeners

---
+++ JavaScript/Score-Keeper/app.js
@@ -0,0 +1,10 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+
+p1Button.addEventListener('click', () => {
+  console.log('click P1');
+});
+
+p2Button.addEventListener('click', () => {
+  console.log('click P2');
+});
