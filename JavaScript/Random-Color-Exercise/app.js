f6e083c2b4e1d7661adc0b636d0a43921e81685b
Jake Mills <jake.mills.05@gmail.com>
2021-03-14 18:45:43 -0400

    Added random number generator function

--- JavaScript/Random-Color-Exercise/app.js
+++ JavaScript/Random-Color-Exercise/app.js
@@ -5,9 +5,15 @@ const body = document.querySelector('body');
 newH1.innerText = 'Click button for a random color!';
 newButton.innerText = 'Click me!';
 
+// Button Event Listener
 newButton.addEventListener('click', () => {
   console.log('Clicked!');
 });
+
+// Function to Randomize Colors
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 body.appendChild(newH1);
 body.appendChild(newButton);
