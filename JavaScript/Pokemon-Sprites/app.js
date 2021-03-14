5596f06e6693a9027589769fc53bcca7a51d1bfd
Jake Mills <jake.mills.05@gmail.com>
2021-03-14 18:01:50 -0400

    Added styling for better display

--- JavaScript/Pokemon-Sprites/app.js
+++ JavaScript/Pokemon-Sprites/app.js
@@ -4,13 +4,14 @@ const container = document.querySelector('#container');
 const baseURL =
   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for (let i = 1; i <= 150; i++) {
+for (let i = 1; i <= 151; i++) {
   const pokemon = document.createElement('div');
   const label = document.createElement('span');
   const newImg = document.createElement('img');
 
   newImg.src = baseURL + `${i}.png`;
   label.innerText = `#${i}`;
+  pokemon.classList.add('pokemon');
 
   pokemon.appendChild(newImg);
   pokemon.appendChild(label);
