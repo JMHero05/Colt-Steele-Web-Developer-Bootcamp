4354c2265e9cd93c817e5931cea394810f713715
Jake Mills <jake.mills.05@gmail.com>
2021-04-15 20:42:31 -0400

    Chained color changing function with .then() to show all the colors of the rainbow...because why not?

--- JavaScript/Creating-Promises/app.js
+++ JavaScript/Creating-Promises/app.js
@@ -31,3 +31,11 @@ const delayedColorChange = (color, delay) => {
     }, delay);
   });
 };
+
+delayedColorChange('red', 1000)
+  .then(() => delayedColorChange('orange', 1000))
+  .then(() => delayedColorChange('yellow', 1000))
+  .then(() => delayedColorChange('green', 1000))
+  .then(() => delayedColorChange('blue', 1000))
+  .then(() => delayedColorChange('indigo', 1000))
+  .then(() => delayedColorChange('violet', 1000));
