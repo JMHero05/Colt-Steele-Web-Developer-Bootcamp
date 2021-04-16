19b144be8145e7836708875fa2c94955714274b7
Jake Mills <jake.mills.05@gmail.com>
2021-04-15 20:22:02 -0400

    Added if statement for resolve/reject logic based upon random number

--- JavaScript/Creating-Promises/app.js
+++ JavaScript/Creating-Promises/app.js
@@ -1,7 +1,11 @@
 const fakeRequest = (url) => {
   return new Promise((resolve, reject) => {
+    const randNum = Math.random();
     setTimeout(() => {
-      resolve();
+      if (randNum < 0.7) {
+        resolve('YOUR FAKE DATA HERE');
+      }
+      reject('Request Error!');
     }, 3000);
   });
 };
