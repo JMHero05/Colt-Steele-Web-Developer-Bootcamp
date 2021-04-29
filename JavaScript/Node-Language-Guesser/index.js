9ad0715a4161864366a3f91d735b2a41995d3a82
Jake Mills <jake.mills.05@gmail.com>
2021-04-29 18:36:15 -0400

    Gave the ability to pass a string to franc from terminal to determine language

--- JavaScript/Node-Language-Guesser/index.js
+++ JavaScript/Node-Language-Guesser/index.js
@@ -1,8 +1,7 @@
 const franc = require('franc');
 const langs = require('langs');
-const langCode = franc(
-  'Hello, World! Codecademy has helped tens of millions of learners upgrade their careers, build meaningful projects, and gain confidence in their skills with engaging, accessible, and flexible online coding education. We provide hands-on, interactive lessons ranging from Python to R to JavaScript and everything in between. Our learners have gone on to start companies, new jobs, and new lives thanks to what they’ve learned with Codecademy.'
-);
+const input = process.argv[2];
+const langCode = franc(input);
 
 const language = langs.where('3', langCode);
 console.log(language.name);
