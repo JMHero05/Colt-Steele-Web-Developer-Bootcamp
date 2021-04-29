bcba2f786b2fb2e5abfda9b0383a78f3a53cdf04
Jake Mills <jake.mills.05@gmail.com>
2021-04-29 18:32:56 -0400

    Created a variable to hold a sentence. Used franc to find the code and then passed the code to langs to use the object to give me the name of the language

--- JavaScript/Node-Language-Guesser/index.js
+++ JavaScript/Node-Language-Guesser/index.js
@@ -1,2 +1,8 @@
 const franc = require('franc');
 const langs = require('langs');
+const langCode = franc(
+  'Hello, World! Codecademy has helped tens of millions of learners upgrade their careers, build meaningful projects, and gain confidence in their skills with engaging, accessible, and flexible online coding education. We provide hands-on, interactive lessons ranging from Python to R to JavaScript and everything in between. Our learners have gone on to start companies, new jobs, and new lives thanks to what they’ve learned with Codecademy.'
+);
+
+const language = langs.where('3', langCode);
+console.log(language.name);
