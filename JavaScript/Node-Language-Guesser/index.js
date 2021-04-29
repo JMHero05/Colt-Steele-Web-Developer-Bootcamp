89584be9febe1fabe6a1de04e674ac9cfc6daa85
Jake Mills <jake.mills.05@gmail.com>
2021-04-29 18:50:22 -0400

    Applied green lettering for successful language findings, and red for unsuccessful findings

--- JavaScript/Node-Language-Guesser/index.js
+++ JavaScript/Node-Language-Guesser/index.js
@@ -1,5 +1,6 @@
 const franc = require('franc');
 const langs = require('langs');
+const colors = require('colors');
 const input = process.argv[2];
 const langCode = franc(input);
 
@@ -8,12 +9,14 @@ function printLanguage() {
   language === undefined
     ? console.log(
         "Sorry, couldn't determine the language from the text provided. Try to enter different text or increase the length of previous text."
+          .red
       )
-    : console.log(`Our best guess is: ${language.name}`);
+    : console.log(`Our best guess is: ${language.name}`.green);
 }
 
 langCode === 'und'
   ? console.log(
       "Sorry, couldn't determine the language from the text provided. Try to enter different text or increase the length of previous text."
+        .red
     )
   : printLanguage();
