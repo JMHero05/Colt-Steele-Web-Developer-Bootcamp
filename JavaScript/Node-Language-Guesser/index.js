e301648636305764ec9eec02cc75499df5f8a3a8
Jake Mills <jake.mills.05@gmail.com>
2021-04-29 18:47:29 -0400

    Added logic to account if franc could not define the language, and logic if langs could not define the code provided by franc

--- JavaScript/Node-Language-Guesser/index.js
+++ JavaScript/Node-Language-Guesser/index.js
@@ -3,5 +3,17 @@ const langs = require('langs');
 const input = process.argv[2];
 const langCode = franc(input);
 
-const language = langs.where('3', langCode);
-console.log(language.name);
+function printLanguage() {
+  const language = langs.where('3', langCode);
+  language === undefined
+    ? console.log(
+        "Sorry, couldn't determine the language from the text provided. Try to enter different text or increase the length of previous text."
+      )
+    : console.log(`Our best guess is: ${language.name}`);
+}
+
+langCode === 'und'
+  ? console.log(
+      "Sorry, couldn't determine the language from the text provided. Try to enter different text or increase the length of previous text."
+    )
+  : printLanguage();
