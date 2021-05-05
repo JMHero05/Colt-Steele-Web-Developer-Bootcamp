5baa0ae7c6033c861a82866ddcbd21031f574185
Jake Mills <jake.mills.05@gmail.com>
2021-05-05 16:35:05 -0400

    Practice with express get requests

--- JavaScript/First-Express-App/index.js
+++ JavaScript/First-Express-App/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use(() => {
-  console.log('WE GOT A NEW REQUEST!');
+// app.use((req, res) => {
+//   console.log('WE GOT A NEW REQUEST!');
+// });
+
+app.get('/', (req, res) => {
+  res.send('THIS IS THE HOMEPAGE');
+});
+
+app.get('/cats', (req, res) => {
+  res.send('MEOW!');
+});
+
+app.get('/dogs', (req, res) => {
+  res.send('WOOF!');
 });
 
 app.listen(3000, () => {
